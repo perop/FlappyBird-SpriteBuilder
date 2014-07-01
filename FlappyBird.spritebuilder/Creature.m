42c42caa249cb8ffc143dc23fc45776fca891b5a
Mai Takahashi <mai.takahashi63@gmail.com>
2014-07-01 11:17:06 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ FlappyBird.spritebuilder/Creature.m
@@ -0,0 +1,13 @@
+//
+//  Creature.m
+//  FlappyBird
+//
+//  Created by 高橋麻衣 on 2014/06/24.
+//  Copyright (c) 2014年 Apportable. All rights reserved.
+//
+
+#import "Creature.h"
+
+@implementation Creature
+
+@end
---
+++ FlappyBird.spritebuilder/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  FlappyBird
+//
+//  Created by 高橋麻衣 on 2014/06/24.
+//  Copyright (c) 2014年 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
